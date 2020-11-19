1f88ce8677585cd1e2bda4923cc4084b5f8b8dba
J <744693884@qq.com>
2020-11-19 20:40:03 +0800

    run.py change name into bool_search.py and realize a basic bool searching function

---
+++ src/bool_search.py
@@ -0,0 +1,45 @@
+'''
+处理50W份邮件，并生成倒排索引表，以及计算td-idf值等
+'''
+
+#进行bool查询
+import pickle
+import sys
+sys.path.append('./src')
+from preprocessing import tokenize
+
+path = './result/allennp_inverted_idx.pkl'
+with open(path,'rb') as file1:
+    inverted_idx = pickle.load(file1)
+
+not_flag = 0
+query_string = input()
+query_or = query_string.split(' or ')
+answer_or = set()
+for query1 in query_or:
+    query_and = query1.split(' and ')# and split
+    answer_and = []
+    for query2 in query_and:
+        query_space = query2.split()# space == 'and'
+        for query3 in query_space:
+            if query3 == 'not':
+                not_flag = 1
+            else:
+                word = tokenize(query3)[0]
+                if not_flag == 1:# not
+                    not_flag = 0 
+                    if (len(answer_and) == 0):
+                        answer_and = [i for i in range(2000) if i not in inverted_idx[word][1]]
+                    else:
+                        answer_and = [i for i in answer_and if i not in inverted_idx[word][1]]
+                else:
+                    if (len(answer_and) == 0):
+                        answer_and = inverted_idx[word][1]
+                    else:
+                        answer_and = [i for i in answer_and if i in inverted_idx[word][1]]
+    if (len(answer_or) == 0):
+        answer_or = set(answer_and)
+    else:
+        answer_or = answer_or.union(set(answer_and)) 
+print(answer_or)
+#每个print 都是对结果的进行and或者or或者not 的处理
